03c437aa7ba744ae83f82900e2fc26e48e97e65f
Doris Chang <salutdoris@gmail>
2021-04-28 17:11:08 +0800

    2nd version-顯示猜的次數

--- guess.py
+++ guess.py
@@ -1,7 +1,9 @@
 import random
 r = random.randint(1,100) # random integer
+count = 0
 
 while True: 
+    count += 1
     a = input('請輸入數字(整數):')
     a = int(a)
     if a == r:
@@ -11,5 +13,5 @@ while True:
         print('數字要小一點')
     elif a < r:
         print('數字要大一點')
-
+    print('你已嘗試', count, '次')
 
