d2ce42ec77ac272476425b129d1d8963948c9145
Doris Chang <salutdoris@gmail>
2021-04-28 17:17:11 +0800

    3rd version: 加入隨機數字上下範圍

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100) # random integer
+start = input('請輸入開始值:')
+end = input('請輸入結束值:')
+start = int(start)
+end = int (end)
+r = random.randint(start,end) # random integer
 count = 0
 
 while True: 
